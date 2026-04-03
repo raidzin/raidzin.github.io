4dfd727b646a7667e83376cc63e77f0b170ec22e
raidzin <agonch2001@gmail.com>
2026-04-03 09:33:23 +0300

    add deploy to github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import mdx from '@astrojs/mdx';
 // https://astro.build/config
 export default defineConfig({
   prefetch: true,
+  site: "https://raidzin.github.io",
   integrations: [mdx()],
   vite: {
     plugins: [tailwindcss()]
